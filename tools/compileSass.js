c5632b9cfa8e924a7927556df644b4a3fb0a0815
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-02-10 14:00:29 +0100

    Provide a minified version as well an keep an eye on file size

--- tools/compileSass.js
+++ tools/compileSass.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const zlib = require("zlib");
 const sass = require("sass-embedded");
+const csso = require("csso");
 
 function compile() {
   let result = sass.compile("./src/css/index.scss", {
@@ -15,5 +17,15 @@ module.exports = compile;
 if (require.main === module) {
   // executed when called directly
   const css = compile();
+  const minifiedCss = csso.minify(css).css;
+  const compressed = zlib.gzipSync(minifiedCss);
   fs.writeFileSync("./dist/angie.css", css);
+  fs.writeFileSync("./dist/angie.min.css", minifiedCss);
+  console.log("Size (ca.) angie.css: ", css.length / 1024.0, "kb");
+  console.log("Size (ca.) angie.min.css: ", minifiedCss.length / 1024.0, "kb");
+  console.log(
+    "Size (ca.) angie.min.css compressed: ",
+    compressed.length / 1024.0,
+    "kb"
+  );
 }
