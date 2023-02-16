827b4fdcfee4ac4c4b390a2d3893b1cac4531129
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-02-16 20:24:26 +0100

    Fix folder/files setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/");
-  eleventyConfig.addPassthroughCopy("docs/fonts.css");
-  eleventyConfig.addPassthroughCopy("assets/fonts/*");
-  eleventyConfig.addPassthroughCopy("assets/svg/*");
   eleventyConfig.addPassthroughCopy("docs/*.css");
   eleventyConfig.addPassthroughCopy("docs/prism.js");
   eleventyConfig.addPassthroughCopy("dist/fonts/*");
+  eleventyConfig.addPassthroughCopy("dist/svg/*");
 
   return {
     dir: {
